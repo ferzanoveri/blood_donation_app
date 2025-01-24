4607b00b584e5301ac299c8ffd4bcd6d5a8bfac4
ferzanoveri <ferzanoveri@gmail.com>
2025-01-24 22:10:53 +0700

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:blood_donation_app/splash.dart';
 import 'package:flutter/material.dart';
+import 'login.dart';
 
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     routes: {
       '/': (context) => Splash(),
-      // '/login': (context) => Login(),
+      '/login': (context) => Login(),
       // '/booking': (context) => Booking(),
       // '/survey': (context) => Survey(),
     },
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class Splash extends StatefulWidget {
@@ -8,10 +10,48 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    startTimer();
+    super.initState();
+  }
+
+  startTimer(){
+    var duration = Duration(seconds: 2);
+    return Timer(
+      duration,
+      route
+    );
+  }
+
+  route(){
+    Navigator.of(context).pushReplacementNamed('login');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image.asset('assets/blood.jpg'),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.transparent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/blood.png',
+                scale: 3,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Blood Donation App',
+                style: TextStyle(fontSize: 35),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
